0b047a55bcd0868e685726003bbcbfb9dddd14f8
Aki Nyrhinen <anyrhine@gmail.com>
2015-03-19 12:15:58 -0700

    better/faster blending, accurate div255, portable version

--- drawrect.c
+++ drawrect.c
@@ -1,117 +1,74 @@
 #include "os.h"
 #include "draw3.h"
 
-typedef struct Samp2d Samp2d;
-struct Samp2d {
-	int u;
-	int u0;
-	int ustep;
-	int uoff;
-	int umod;
-	int ustride;
+#define add_wrap(ptr, d, start, end) ptr = ptr+d; ptr = ptr < end ? ptr : start
 
-	int v;
-	int v0;
-	int vstep;
-	int voff;
-	int vmod;
-	int vstride;
-};
+static inline u32int
+blend32(u32int dval, u32int sval, u32int mval)
+{
+	u32int dtmp, stmp, tmp1, tmp2;
 
-static inline void
-initsamp(
-	Samp2d *samp,
-	int uoff, int u0, int ustep, int umod, int ustride,
-	int voff, int v0, int vstep, int vmod, int vstride
-){
-	samp->u0 = ustride*u0;
-	samp->umod = ustride*umod;
-	samp->ustep = ustride*ustep;
-	samp->uoff = ustride*(u0 + (uoff - u0) % umod);
-	samp->u = samp->uoff;
-	samp->ustride = ustride;
+	/* multiply red and blue at the same time */
+	dtmp = dval & 0x00ff00ff;
+	stmp = sval & 0x00ff00ff;
+	tmp1 = dtmp * (255-mval);
+	tmp1 += stmp * mval;
 
-	samp->v0 = vstride*v0;
-	samp->vmod = vstride*vmod;
-	samp->vstep = vstride*vstep;
-	samp->voff = vstride*(v0 + (voff - v0) % vmod);
-	samp->v = samp->voff;
-	samp->vstride = vstride;
+	/* divide by 255 */
+	tmp1 += 0x00010001;
+	tmp1 += (tmp1>>8) & 0x00ff00ff;
+	tmp1 >>= 8;
+	tmp1 = tmp1 & 0x00ff00ff; 
+
+	/* green goes alone */
+	dtmp = dval & 0xff00;
+	stmp = sval & 0xff00;
+	tmp2 = dtmp * (255-mval);
+	tmp2 += stmp * mval;
+
+	/* divide by 255 */
+	tmp2 += 0x0100;
+	tmp2 += (tmp1>>8);
+	tmp2 >>= 8;
+	tmp2 = tmp2 & 0xff00;
+
+	return tmp1 | tmp2;
 }
 
-static inline void
-uresetsamp(Samp2d *samp)
+static inline u32int *
+img_uvstart(Image *img, int uoff, int voff)
 {
-	samp->u = samp->uoff;
+	int u0, v0;
+	u0 = img->r.u0 + (uoff % rectw(&img->r));
+	v0 = img->r.v0 + (voff % recth(&img->r));
+	return (u32int*)(img->img + img->stride*v0 + 4*u0);
 }
 
-static inline void
-ustepsamp(Samp2d *samp)
+static inline u32int *
+img_vstart(Image *img, int uoff)
 {
-//samp->u = samp->u0 + (samp->u + samp->ustep - samp->u0)%samp->umod;
-	int a, b, m;
-	a = samp->u + samp->ustep - samp->u0;
-	b = samp->u + samp->ustep - samp->u0 - samp->umod;
-	m = b>>31;
-	samp->u = samp->u0 + ((a&m)|(b&~m));
+	int u0, v0;
+	u0 = img->r.u0 + (uoff % rectw(&img->r));
+	v0 = img->r.v0;
+	return (u32int*)(img->img + img->stride*v0 + 4*u0);
 }
 
-static inline void
-vstepsamp(Samp2d *samp)
+static inline u32int *
+img_end(Image *img)
 {
-//samp->v = samp->v0 + (samp->v + samp->vstep - samp->v0)%samp->vmod;
-	int a, b, m;
-	a = samp->v + samp->vstep - samp->v0;
-	b = samp->v + samp->vstep - samp->v0 - samp->vmod;
-	m = b>>31;
-	samp->v = samp->v0 + ((a&m)|(b&~m));
-}
-
-static inline uchar *
-imagepix(Image *img, short u, short v)
-{
-	u = img->r.u0 + (u - img->r.u0) % rectw(&img->r);
-	v = img->r.v0 + (v - img->r.v0) % recth(&img->r);
-	return img->img + img->stride*v + 4*u;
-}
-
-
-static inline uchar
-blend1(uchar dst, uchar src, uchar a)
-{
-//	return (dst*(255-a) + src*a)/255;
-
-	u16int tmp;
-	tmp = (dst*(255-a) + src*a);
-	return (tmp*257+256)>>16;
-//	return ((tmp+1)*257)>>16;
-//	tmp += 1; return (tmp+(tmp>>8))>>8;
-}
-
-typedef int s32vec __attribute__((vector_size(16)));
-
-static inline void
-blend4(uchar *dst, uchar *src, uchar *mask)
-{
-	s32vec svec = (s32vec){src[0], src[1], src[2], src[3]};
-	s32vec mvec = (s32vec){mask[0], mask[1], mask[2], mask[3]};
-	s32vec dvec = (s32vec){dst[0], dst[1], dst[2], dst[3]};
-
-	dvec = (dvec*(255-mvec) + svec*mvec)/255;
-
-	dst[0] = dvec[0];
-	dst[1] = dvec[1];
-	dst[2] = dvec[2];
-	dst[3] = dvec[3];
+	return (u32int*)(img->img + img->stride*img->r.vend);
 }
 
 void
 drawblend(Image *dst, Rect r, Image *src, Image *mask)
 {
-	Samp2d msamp, ssamp;
 	Rect dstr;
-	uchar *dstp, *srcp, *maskp;
-	short u, v;
+	int uoff, voff;
+
+#if 0
+	double st, et;
+	st = timenow();
+#endif
 
 	dst->dirty = 1;
 	dstr = cliprect(r, dst->r);
@@ -119,43 +76,94 @@ drawblend(Image *dst, Rect r, Image *src, Image *mask)
 	if(dstr.u0 >= dstr.uend || dstr.v0 >= dstr.vend)
 		return;
 
-	initsamp(
-		&ssamp,
-		dstr.u0-r.u0, src->r.u0, 1, rectw(&src->r), 4,
-		dstr.v0-r.v0, src->r.v0, 1, recth(&src->r), src->stride
-	);
+	uoff = dstr.u0-r.u0;
+	voff = dstr.v0-r.v0;
 
-	initsamp(
-		&msamp,
-		dstr.u0-r.u0, mask->r.u0, 1, rectw(&mask->r), 4,
-		dstr.v0-r.v0, mask->r.v0, 1, recth(&mask->r), mask->stride
-	);
+	u32int *dstp, *srcp, *maskp;
+	u32int *src_vstart, *mask_vstart;
+	u32int *dst_end, *src_end, *mask_end;
+	int src_uendoff, mask_uendoff;
+	int dst_stride, src_stride, mask_stride;
+	u32int *dst_ustart, *src_ustart, *mask_ustart;
+	u32int *dst_uend, *src_uend, *mask_uend;
+	u32int mval;
 
-	for(v = dstr.v0; v < dstr.vend; v++){
-		uresetsamp(&ssamp);
-		uresetsamp(&msamp);
-		for(u = dstr.u0; u < dstr.uend; u++){
-			dstp = dst->img + 4*u + dst->stride*v;
-			srcp = src->img + ssamp.u + ssamp.v;
-			maskp = mask->img + msamp.u + msamp.v;
-			blend4(dstp, srcp, maskp);
-			ustepsamp(&ssamp);
-			ustepsamp(&msamp);
+	src_vstart = img_vstart(src, uoff);
+	mask_vstart = img_vstart(mask, uoff);
+
+	dst_ustart = img_uvstart(dst, dstr.u0, dstr.v0);
+	src_ustart = img_uvstart(src, uoff, voff);
+	mask_ustart = img_uvstart(mask, uoff, voff);
+
+	__builtin_prefetch(dst_ustart);
+	__builtin_prefetch(src_ustart);
+	__builtin_prefetch(mask_ustart);
+	__builtin_prefetch(dst_ustart+8);
+	__builtin_prefetch(src_ustart+8);
+	__builtin_prefetch(mask_ustart+8);
+
+	dst_stride = dst->stride/4;
+	src_stride = src->stride/4;
+	mask_stride = mask->stride/4;
+
+	dst_end = dst_ustart + recth(&dstr)*dst_stride;
+	src_end = img_end(src);
+	mask_end = img_end(mask);
+
+	dst_uend = dst_ustart + rectw(&dstr);
+	src_uendoff = rectw(&src->r) - uoff;
+	mask_uendoff = rectw(&mask->r) - uoff;
+
+	while(dst_ustart < dst_end){
+
+		maskp = mask_ustart;
+		mask_uend = mask_ustart + mask_uendoff;
+
+		srcp = src_ustart;
+		src_uend = src_ustart + src_uendoff;
+
+		dstp = dst_ustart;
+		while(dstp < dst_uend){
+
+			__builtin_prefetch(dstp+16);
+			__builtin_prefetch(srcp+16);
+			__builtin_prefetch(maskp+16);
+
+			/* the secret to fast alpha blending is avoiding it */
+			mval = *maskp >> 24;
+			if(__builtin_expect(mval-1 < 254, 0)){
+				*dstp = blend32(*dstp, *srcp, mval);
+			} else if(mval == 255){
+				*dstp = *srcp;
+			}
+
+			add_wrap(maskp, 1, mask_ustart, mask_uend);
+			add_wrap(srcp, 1, src_ustart, src_uend);
+			dstp++;
 		}
-		vstepsamp(&ssamp);
-		vstepsamp(&msamp);
-	}
-}
 
+		add_wrap(mask_ustart, mask_stride, mask_vstart, mask_end);
+		add_wrap(src_ustart, src_stride, src_vstart, src_end);
+
+		dst_ustart += dst_stride;
+		dst_uend += dst_stride;
+	}
+
+#if 0
+	et = timenow();
+int nbytes = 4*rectw(&dstr)*recth(&dstr);
+if(fmod(et, 1.0) < 0.1)
+fprintf(stderr, "drawblend %d bytes, %.2f MB/s\n", nbytes, 1e-6*nbytes/(et-st));
+#endif
+}
 
 void
 drawrect(Image *dst, Rect r, uchar *color)
 {
 	Rect dstr;
-
-	short u, v;
-	u32int *pix, cval;
-	int uoff, voff;
+	uchar *dstp;
+	int i, nbytes;
+	int dst_stride;
 
 	dstr = cliprect(r, dst->r);
 
@@ -163,16 +171,11 @@ drawrect(Image *dst, Rect r, uchar *color)
 		return;
 
 	dst->dirty = 1;
-	cval = *(u32int*)color;
-	uoff = dstr.u0*4;
-	voff = dstr.v0*dst->stride;
-	for(v = dstr.v0; v < dstr.vend; v++){
-		uoff = dstr.u0*4;
-		for(u = dstr.u0; u < dstr.uend; u++){
-			pix = (u32int*)(dst->img + voff + uoff);
-			*pix = cval;
-			uoff += 4;
-		}
-		voff += dst->stride;
+	dst_stride = dst->stride;
+	dstp = dst->img + dst_stride*dstr.v0 + 4*dstr.u0;
+	nbytes = 4*rectw(&dstr);
+	for(i = dstr.v0; i < dstr.vend; i++){
+		pixset(dstp, color, nbytes);
+		dstp += dst->stride;
 	}
 }
