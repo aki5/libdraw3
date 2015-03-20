77b25ac287fc2a0d51f3e0e0b6eb535d258b9e5d
Aki Nyrhinen <anyrhine@gmail.com>
2015-03-19 19:21:22 -0700

    bring the non-simd version up to date

--- drawtri.c
+++ drawtri.c
@@ -2,57 +2,70 @@
 #include "draw3.h"
 
 static inline void
-drawpixel(uchar *img, int width, short x, short y, u32int color, u32int mask)
+fragment(Image *img, short x0, short y0, u32int color)
 {
 	u32int *pix;
-	int off = y*width+x;
-	pix = (u32int *)img+off;
-	if(!mask) *pix = color;
+	int off = y0*img->stride + x0*4;
+	pix = (u32int *)(img->img + off);
+	pix[0] = color;
 }
 
-static void
-bbox_clamp(short *xsp, short *ysp, short *xep, short *yep, int width, int height, short *a, short *b, short *c, int subpix)
+/*
+ *	the top-left rule: move a bottom edge up and the right edge left
+ */
+static inline int
+topleft(short *a, short *b)
 {
-	*xsp = maxi(0, mini(a[0], mini(b[0], c[0]))) >> subpix;
-	*ysp = maxi(0, mini(a[1], mini(b[1], c[1]))) >> subpix;
-	*xep = ((mini((width<<subpix)-1, maxi(a[0], maxi(b[0], c[0])))+(1<<subpix)-1) >> subpix) + 1;
-	*yep = ((mini((height<<subpix)-1, maxi(a[1], maxi(b[1], c[1])))+(1<<subpix)-1) >> subpix) + 1;
+	if(a[1] == b[1] && a[0] < b[0]) /* bottom */
+		return -1;
+	if(a[1] > b[1]) /* right */
+		return -1;
+	return 0; /* top or left */
 }
 
-void
-drawtri(uchar *img, int width, int height, short *a, short *b, short *c, uchar *color, int subpix)
+static inline void
+drawtri_horse(Image *img, Rect *r, short *a, short *b, short *c, uchar *color)
 {
 	int abp_y, bcp_y, cap_y, abp, bcp, cap;
 	int abp_dx, bcp_dx, cap_dx;
 	int abp_dy, bcp_dy, cap_dy;
-	short azero, bzero, czero;
-	short xstart, xend;
-	short ystart, yend;
-	short p[2];
+	int mask;
 
-	bbox_clamp(&xstart, &ystart, &xend, &yend, width, height, a, b, c, subpix);
+	short u0, v0;
 
-	p[0] = xstart << subpix;
-	p[1] = ystart << subpix;
+	abp_dx = ori2i_dx(a, b);
+	bcp_dx = ori2i_dx(b, c);
+	cap_dx = ori2i_dx(c, a);
 
+	abp_dy = ori2i_dy(a, b);
+	bcp_dy = ori2i_dy(b, c);
+	cap_dy = ori2i_dy(c, a);
+
+	short p[2] = { r->u0, r->v0 };
 	abp_y = ori2i(a, b, p);
 	bcp_y = ori2i(b, c, p);
 	cap_y = ori2i(c, a, p);
 
-	abp_dx = ori2i_dx(a, b) << subpix;
-	bcp_dx = ori2i_dx(b, c) << subpix;
-	cap_dx = ori2i_dx(c, a) << subpix;
+	abp_y += topleft(a, b);
+	bcp_y += topleft(b, c);
+	cap_y += topleft(c, a);
 
-	abp_dy = ori2i_dy(a, b) << subpix;
-	bcp_dy = ori2i_dy(b, c) << subpix;
-	cap_dy = ori2i_dy(c, a) << subpix;
+	abp_dx = abp_dx;
+	bcp_dx = bcp_dx;
+	cap_dx = cap_dx;
 
-	for(p[1] = ystart; p[1] < yend; p[1]++){
+	abp_dy = abp_dy;
+	bcp_dy = bcp_dy;
+	cap_dy = cap_dy;
+
+	for(v0 = r->v0; v0 < r->vend; v0 += 1){
 		abp = abp_y;
 		bcp = bcp_y;
 		cap = cap_y;
-		for(p[0] = xstart; p[0] < xend; p[0]++){
-			drawpixel(img, width, p[0], p[1], *(u32int*)color, (abp|bcp|cap)>>31);
+		for(u0 = r->u0; u0 < r->uend; u0 += 1){
+			mask = (abp | bcp | cap) >> 31;
+			if(mask == 0)
+				fragment(img, u0, v0, *(u32int*)color);
 			abp += abp_dx;
 			bcp += bcp_dx;
 			cap += cap_dx;
@@ -63,16 +76,21 @@ drawtri(uchar *img, int width, int height, short *a, short *b, short *c, uchar *
 	}
 }
 
-void
-drawtris(uchar *img, int width, int height, short *tris, uchar *colors, int ntris, int subpix)
+
+static void
+rect_trisetup(Rect *r, short *a, short *b, short *c)
 {
-	int i;
-	for(i = 0; i < ntris; i++){
-		short *a, *b, *c;
-		a = tris+6*i+0;
-		b = tris+6*i+2;
-		c = tris+6*i+4;
-		drawtri(img, width, height, a, b, c, colors+4*i, subpix);
-	}
+	r->u0 = maxi(r->u0, mini(a[0], mini(b[0], c[0])));
+	r->v0 = maxi(r->v0, mini(a[1], mini(b[1], c[1])));
+	r->uend = mini(r->uend-1, maxi(a[0], maxi(b[0], c[0]))) + 1;
+	r->vend = mini(r->vend-1, maxi(a[1], maxi(b[1], c[1]))) + 1;
 }
 
+void
+drawtri(Image *img, Rect r, short *a, short *b, short *c, uchar *color)
+{
+	img->dirty = 1;
+	r = cliprect(r, img->r);
+	rect_trisetup(&r, a, b, c);
+	drawtri_horse(img, &r, a, b, c, color);
+}
