dc0d8df866848e6313d4946845e6105def488086
Aki Nyrhinen <anyrhine@gmail.com>
2015-03-21 21:03:14 -0700

    iabs and signed integer types

--- os.h
+++ os.h
@@ -13,6 +13,9 @@ typedef unsigned int u32int;
 typedef unsigned long long u64int;
 typedef uintptr_t uintptr;
 
+typedef int s32int;
+typedef long long s64int;
+
 #define nelem(x) (int)(sizeof(x)/sizeof(x[0]))
 
 static inline double
@@ -21,4 +24,10 @@ timenow(void)
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
 	return (double)tv.tv_sec + 1e-6*tv.tv_usec;
+}
+
+static inline int
+iabs(int x)
+{
+	return x < 0 ? -x : x;
 }
