a764c16dc63d0722e1d785655e02c2c38c5345d8
Aki Nyrhinen <anyrhine@gmail.com>
2015-03-22 10:22:10 -0700

    rcp32 with s64int, make rcp32&rcp17 take and return unsigned

--- misc/newton.c
+++ misc/newton.c
@@ -2,14 +2,16 @@
 #include "os.h"
 
 enum {
-	Newtpsh = 16,
+	Newtpsh = 32,
 };
 
-static inline s32int
-rcp32(s32int pshift, s32int q)
+u32int
+rcp17(int pshift, u32int q)
 {
-
 	s32int tag, z;
+
+	if(q == 0)
+		return -1;
 
 	z = 1 << (pshift-(31-__builtin_clz(q)));
 	tag = q >> ((31-2)-__builtin_clz(q));
@@ -19,24 +21,51 @@ rcp32(s32int pshift, s32int q)
 	if(tag == 5 || tag == 6 || q == 3)
 		z = (z|(z>>1))>>1;
 
-	z = z + ((z*((1<<pshift)-q*z))>>pshift);
-	z = z + ((z*((1<<pshift)-q*z))>>pshift);
-	z = z + ((z*((1<<pshift)-q*z))>>pshift);
+	z = z + (((z<<pshift)-q*z*z)>>pshift);
+	z = z + (((z<<pshift)-q*z*z)>>pshift);
+	z = z + (((z<<pshift)-q*z*z)>>pshift);
 
 	return z;
 }
 
+u32int
+rcp32(int pshift, u32int q)
+{
+	s32int tag;
+	s64int z;
+
+	if(q == 0)
+		return -1;
+
+	z = ((s64int)1 << (pshift-(63-__builtin_clzl(q))));
+	tag = q >> ((63-2)-__builtin_clzl(q));
+	if(tag == 7)
+		z = z>>1;
+	if(tag == 5 || tag == 6 || q == 3)
+		z = (z|(z>>1))>>1;
+
+	z = z + (((z<<pshift)-q*z*z)>>pshift);
+	z = z + (((z<<pshift)-q*z*z)>>pshift);
+	z = z + (((z<<pshift)-q*z*z)>>pshift);
+
+	return z;
+}
+
+
 int
 main(void)
 {
-	double err;
-	int x, tag;
-	for(x = 1; x < (1<<Newtpsh); x++){
-		int dd = (1<<Newtpsh)/x;
-		int newt = rcp32(Newtpsh, x);
+	double err, maxerr;
+	s64int x;
+	maxerr = 0.0;
+	for(x = 4; x < ((s64int)1<<Newtpsh); x++){
+
+		int dd = ((s64int)1<<Newtpsh)/x;
+		s64int newt = rcp32(Newtpsh, x);
 		err = fabs(1.0*(dd-newt)/dd);
 		if(iabs(dd-newt)>1){// && err > maxerr){
-			printf("result %7d/%-5d div %-6d newton %-6d tag %d error %6.2f %%\n", (1<<Newtpsh), x, dd, newt, tag, 100.0*err);
+			maxerr = maxerr > err ? maxerr : err;
+			printf("result %7lld/%-5lld div %-6d newton %-6lld error %6.2f %% maxerr %6.2f %%\n", ((s64int)1<<Newtpsh), x, dd, newt, 100.0*err, 100.0*maxerr);
 		}
 	}
 	return 0;
