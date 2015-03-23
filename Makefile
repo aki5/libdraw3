9da11ff507c5af5b704a49663d04046472dcfcce
Aki Nyrhinen <anyrhine@gmail.com>
2015-03-23 13:21:00 -0700

    make deps work, make paths relative to root, etc

--- Makefile
+++ Makefile
@@ -1,14 +1,13 @@
+ROOT=..
 
 CFLAGS=-O3 -fomit-frame-pointer -W -Wall
-#CFLAGS=-g -W -Wall
+all: sqpool
 
-all: libdraw3.a
+include libdraw3.mk
 
-sqpool: sqpool.c libdraw3.a
-	$(CC) -DTEST $(CFLAGS) -o $@ sqpool.c ./libdraw3.a -lX11 -lXext -lm `freetype-config --libs`
+sqpool: sqpool.c $(LIBDRAW3)
+	$(CC) -DTEST $(CFLAGS) -o $@ sqpool.c $(LIBDRAW3) -lX11 -lXext -lm `freetype-config --libs`
 
 clean:
 	rm -f *.a *.o perf.* sqpool
 
-R:=.
-include libdraw3.mk
--- libdraw3.mk
+++ libdraw3.mk
@@ -1,36 +1,39 @@
 
+LIBDRAW3=$(ROOT)/libdraw3/libdraw3.a
+LIBDRAW3_LIBS=-lX11 -lXext -lm `freetype-config --libs`
+
 DRAW3_HFILES=\
-	$(R)/os.h\
-	$(R)/draw3.h\
-	$(R)/rectpool.h\
-	$(R)/sqpool.h\
-	$(R)/magicu.h\
-	$(R)/imgtools.h\
+	$(ROOT)/libdraw3/os.h\
+	$(ROOT)/libdraw3/draw3.h\
+	$(ROOT)/libdraw3/rectpool.h\
+	$(ROOT)/libdraw3/sqpool.h\
+	$(ROOT)/libdraw3/magicu.h\
+	$(ROOT)/libdraw3/imgtools.h\
 
 DRAW3_OFILES=\
-	$(R)/drawx11.o\
-	$(R)/keysym2ucs.o \
-	$(R)/drawpoly.o\
-	$(R)/drawrect.o\
-	$(R)/rectpool.o\
-	$(R)/loadimage.o\
-	$(R)/sqpool.o\
-	$(R)/drawstr.o\
-	$(R)/drawtri.o\
-	$(R)/drawcircle.o\
-	$(R)/drawellipse.o\
-	$(R)/port.o\
-	#$(R)/arm6.o\
-	#$(R)/magicu.o\
-	#$(R)/drawtri_simd.o\
+	$(ROOT)/libdraw3/drawx11.o\
+	$(ROOT)/libdraw3/keysym2ucs.o \
+	$(ROOT)/libdraw3/drawpoly.o\
+	$(ROOT)/libdraw3/drawrect.o\
+	$(ROOT)/libdraw3/rectpool.o\
+	$(ROOT)/libdraw3/loadimage.o\
+	$(ROOT)/libdraw3/sqpool.o\
+	$(ROOT)/libdraw3/drawstr.o\
+	$(ROOT)/libdraw3/drawtri.o\
+	$(ROOT)/libdraw3/drawcircle.o\
+	$(ROOT)/libdraw3/drawellipse.o\
+	$(ROOT)/libdraw3/port.o\
+	#$(ROOT)/libdraw3/arm6.o\
+	#$(ROOT)/libdraw3/magicu.o\
+	#$(ROOT)/libdraw3/drawtri_simd.o\
 
-$(R)/%.o: $(R)/%.c
-	$(CC) -I$(R) $(CFLAGS) -c -o $@ $<
+$(ROOT)/libdraw3/%.o: $(ROOT)/libdraw3/%.c
+	$(CC) -I$(ROOT)/libdraw3 $(CFLAGS) -c -o $@ $<
 
-$(R)/drawstr.o: $(R)/drawstr.c
+$(ROOT)/libdraw3/drawstr.o: $(ROOT)/libdraw3/drawstr.c
 	$(CC) $(CFLAGS) `freetype-config --cflags` -o $@ -c $<
 
-libdraw3.a: $(DRAW3_OFILES)
-	$(AR) r $@  $(DRAW3_OFILES)
+$(LIBDRAW3): $(DRAW3_OFILES)
+	$(AR) -rv $@  $(DRAW3_OFILES)
 
-$(DRAW_OFILES): $(DRAW_HFILES)
+$(DRAW3_OFILES): $(DRAW3_HFILES)
